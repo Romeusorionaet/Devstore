2cbebfa7c1d82442993a4d41afed98a478025f39
romeusorionaet <romeusoares14569@gmail.com>
2023-12-22 15:42:44 -0300

    setup cypress and testing cart and search

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,37 @@
+describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should be able to navigate to the product page and it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated products on cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.searchByQuery('moletom')
+
+    cy.get('a[href^="/product"]').contains('Moletom Java 2023').click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+})
--- src/app/components/cart-widget.tsx
+++ src/app/components/cart-widget.tsx
@@ -8,7 +8,7 @@ export function CartWidget() {
   return (
     <div className="flex items-center gap-2">
       <ShoppingBag className="h-4 w-4" />
-      <span className="text-sm">Cart {items.length}</span>
+      <span className="text-sm">Cart ({items.length})</span>
     </div>
   )
 }
